c06e4b52e30f7ab65d9925e086bf307e8faa8ea4
DavidPereira976 <davidpereira2002rj@gmail.com>
2025-04-06 18:12:03 -0300

    add:conection with api and exibition o

--- src/components/Pages/LivrosDoados/livrosDoados.jsx
+++ src/components/Pages/LivrosDoados/livrosDoados.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import S from "../LivrosDoados/livrosDoados.module.scss";
 import Book1 from "../../../assets/DonateBook1.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function LivrosDoados() {
+
+ const[livros, setLivros] = useState([]);
+
+ const getLivros = async () => {
+    const response = await axios.get("https://livros-api-python-flask-1.onrender.com/livros");
+    setLivros(response.data);
+ }
+
+ useEffect(()=>{
+  getLivros();
+ },[]);
+
+
   return (
     <div>
       <section className={S.sectionDonate}>
@@ -12,6 +27,13 @@ export default function LivrosDoados() {
           <p>Sussane Andrade</p>
           <p>Ficção</p>
         </div>
+        {livros.map((item)=>(
+          <article>
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
       </section>
     </div>
   );
--- src/components/Pages/QueroDoar/queroDoar.jsx
+++ src/components/Pages/QueroDoar/queroDoar.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import S from "./queroDoar.module.scss";
 
-export default function queroDoar() {
+export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [imagem_url, setImagem_url] = useState("");
+
+  const enviarDados = async () => {
+    const urlApi = "https://livros-api-python-flask-1.onrender.com/doar";
+
+    const dadosEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url,
+    };
+
+    await axios.post(urlApi, dadosEnviar);
+
+    alert("Livro doado com sucesso!");
+
+    setTitulo("");
+    setCategoria("");
+    setAutor("");
+    setImagem_url("");
+  };
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value);
+  };
+
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const capturaAutor = (e) => {
+    setAutor(e.target.value);
+  };
+
+  const capturaImagem_url = (e) => {
+    setImagem_url(e.target.value);
+  };
   return (
-    <div>
-      <h1>Quero doar teste</h1>
-    </div>
-  )
+    <section className={S.principal}>
+      <section className={S.container}>
+        <h2>
+          Por favor, preencha o formulário com suas informações e as informações
+          do Livro
+        </h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            {/* <img src={livro} alt="icone de um livro" /> */}
+            <h3>Informações do Livro</h3>
+          </div>
+          <input
+            type="text"
+            placeholder="Titulo"
+            onChange={capturaTitulo}
+            value={titulo}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            onChange={capturaCategoria}
+            value={categoria}
+          />
+          <input
+            type="text"
+            placeholder="Autor"
+            onChange={capturaAutor}
+            value={autor}
+          />
+          <input
+            type="text"
+            placeholder="Link da Imagem"
+            onChange={capturaImagem_url}
+            value={imagem_url}
+          />
+          <input type="submit" value="Doar" onClick={enviarDados} />
+        </form>
+      </section>
+    </section>
+  );
 }
